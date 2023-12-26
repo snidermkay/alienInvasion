47e86dd54b1afa524f40567dd36dacc3400f36f7
Kira <snidermk.sei@gmail.com>
2023-12-26 16:05:09 -0500

    Ending the game - identifying when parts of the game should runn

--- alien_invasion.py
+++ alien_invasion.py
@@ -43,9 +43,12 @@ class AlienInvasion:
         while True:
             # Watch for keyboard and mouse events.
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
             
         # Redraw the screen during each pass through the loop.
