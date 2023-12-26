2e6026ae22c9f04bae5988da9425e3366b7ce1f7
Kira <snidermk.sei@gmail.com>
2023-12-26 15:22:39 -0500

    Shooting aliens - making larger bullets for testing

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ class Settings:
 
         # Bullet settings
         self.bullet_speed = 1.0
-        self.bullet_width = 3
+        self.bullet_width = 3000
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
@@ -23,4 +23,3 @@ class Settings:
         self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
-        
